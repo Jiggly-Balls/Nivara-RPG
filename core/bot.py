0efe3ca8e6e42f0b6c280d8c1e9d4ebe7eb3bae1
Jiggly Balls <jigglyballs9000@gmail.com>
2025-04-25 19:33:36 +0530

    Integrated logging

--- core/bot.py
+++ core/bot.py
@@ -1,20 +1,28 @@
+from __future__ import annotations
+
 import datetime
+import logging
+from typing import TYPE_CHECKING
 
 import discord
 
 from backend.cache import Cache
 
+if TYPE_CHECKING:
+    from discord import Intents
+
+
+logger = logging.getLogger(__name__)
+
 
 class Bot(discord.Bot):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, *, intents: Intents) -> None:
+        super().__init__(intents=intents)
 
     async def on_ready(self) -> None:
         Cache.last_reconnect = (
             f"<t:{round(datetime.datetime.now().timestamp())}:R>"
         )
 
-        print("\n============================\n")
-        print(f"Logged in as :: {self.user}")
-        print("Your life is meaningless.")
-        print("\n============================\n")
+        logging.info(f"Logged in as :: {self.user}")
+        logging.info("Your life is meaningless.")
