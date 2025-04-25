5a5caa18dda589f516ee26d336ea3ad0e16f1564
Jiggly Balls <jigglyballs9000@gmail.com>
2025-04-25 22:35:39 +0530

    Added database errors

---
+++ backend/errors.py
@@ -0,0 +1,66 @@
+from typing import Any
+
+
+class DBException(Exception):
+    """Base class of the database exceptions."""
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+
+
+class DBConnectionException(DBException):
+    """Raised when database connection issue arises.
+
+    Attributes
+    ------------
+    error_code: :class:`int`
+        The error code associated with connection exception.
+    """
+
+    def __init__(self, *args: Any, error_code: int, **kwargs: Any) -> None:
+        self.error_code = error_code
+        super().__init__(*args, **kwargs)
+
+
+class InvalidType(DBException):
+    """Raised when an invalid type is passed.
+
+    Attributes
+    ------------
+    val_type: :type:`type`
+        The invalid type that was passed.
+
+    expected_type: :type:`type`
+        The type that was expected.
+    """
+
+    def __init__(
+        self, *args: Any, val_type: type, expected_type: type, **kwargs: Any
+    ) -> None:
+        self.val_type = val_type
+        self.expected_type = expected_type
+        super().__init__(*args, **kwargs)
+
+
+class AirtableException(Exception):
+    """Base class of Airtable exceptions"""
+
+
+class InvalidResponse(AirtableException):
+    """Raised when a non 2xx response code is sent.
+
+    Attributes
+    ------------
+    code: :class:`int`
+        The error code.
+
+    status: :class:`str`
+        The error message.
+    """
+
+    def __init__(
+        self, *args: Any, code: int, status: str, **kwargs: Any
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self.code = code
+        self.status = status
