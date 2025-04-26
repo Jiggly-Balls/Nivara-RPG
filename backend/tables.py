a6940f3e6d8f6f47a1e650b6f93a0874d5bd99c8
Jiggly Balls <jigglyballs9000@gmail.com>
2025-04-26 20:00:16 +0530

    Added Base & User table

---
+++ backend/tables.py
@@ -0,0 +1,17 @@
+from sqlalchemy import BigInteger, Column, Integer
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase
+
+
+class BaseTable(AsyncAttrs, DeclarativeBase): ...
+
+
+class User(BaseTable):
+    __tablename__ = "users"
+
+    id = Column(BigInteger, primary_key=True)
+    level = Column(Integer, default=0)
+    exp = Column(Integer, default=0)
+    wallet = Column(Integer, default=1000)
+    bank = Column(Integer, default=0)
+    net_worth = Column(Integer, default=1000)
