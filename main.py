2ba1d4d7c0c90a76996f446e9998070dba370c26
Jiggly Balls <jigglyballs9000@gmail.com>
2025-04-26 20:01:08 +0530

    Improved QOL & integrated SQLAlchemy

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+"""
+Nivara RPG
+~~~~~~~~~~
+
+A fun RPG bot for your discord server.
+
+:copyright: (c) 2025-present Jiggly Balls
+:license: MIT License, see LICENSE for more details.
+"""
+
+__title__ = "Nivara RPG"
+__author__ = "Jiggly Balls"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2025-present Jiggly Balls"
+
+
 import asyncio
 import datetime
 import logging
@@ -5,20 +21,18 @@ import os
 
 import discord
 import dotenv
+from discord import MISSING
+from sqlalchemy.ext.asyncio import create_async_engine
 
 from backend.base_db import BaseData
-from backend.base_pg import create_base_pool_connection
 from backend.cache import Cache
+from backend.tables import BaseTable
 from core import Bot, setup_logging
 from data.constants.core import EXTENSION_DIRECTORY
 
 ENV = dotenv.dotenv_values(".env")
 TOKEN = ENV["TOKEN"]
-DB_HOST = ENV["DB_HOST"]
-DB_PASSWORD = ENV["DB_PASSWORD"]
-DB_NAME = ENV["DB_NAME"]
-DB_PORT = ENV["DB_PORT"]
-DB_USER = ENV["DB_USER"]
+CONNECTION_STRING = ENV["CONNECTION_STRING"]
 intents = discord.Intents.none()
 
 
@@ -45,27 +59,33 @@ def load_extensions(bot: Bot) -> None:
 
 
 async def main() -> None:
-    async with create_base_pool_connection(
-        host=DB_HOST,
-        database=DB_NAME,
-        port=DB_PORT,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        max_inactive_connection_lifetime=0,
-    ) as pool:
-        logger.info("Connected to database")
+    try:
+        if CONNECTION_STRING is None:
+            raise RuntimeError(
+                "No 'CONNECTION_STRING' was provided in the .env file"
+            )
+
+        if TOKEN is None:
+            raise RuntimeError("No 'TOKEN' was provided in the .env file")
 
         bot = Bot(intents=intents)
-        BaseData.db_connection = pool
+        BaseData.db_engine = create_async_engine(CONNECTION_STRING)
         Cache.uptime = f"<t:{round(datetime.datetime.now().timestamp())}:R>"
+
+        async with BaseData.db_engine.begin() as conn:
+            await conn.run_sync(BaseTable.metadata.create_all)
 
         load_extensions(bot=bot)
 
-        if TOKEN is None:
-            raise RuntimeError("No token was provided in the .env file.")
-
         await bot.start(TOKEN)
+
+    finally:
+        if BaseData.db_engine is not MISSING:
+            await BaseData.db_engine.dispose()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.info("Exited due to keyboard interrupt.")
