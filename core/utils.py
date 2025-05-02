b73fee5f90d423e1a227a5f5fb12edb6d776d97f
Jiggly Balls <jigglyballs9000@gmail.com>
2025-05-02 20:22:46 +0530

    Added error embed

--- core/utils.py
+++ core/utils.py
@@ -4,7 +4,7 @@ from typing import Any, overload
 
 from discord import Embed
 
-from data.constants.core import PRIMARY_COLOUR
+from data.constants.core import ERROR_COLOUR, PRIMARY_COLOUR
 
 
 class MainEmbed(Embed):
@@ -27,5 +27,29 @@ class MainEmbed(Embed):
             title=title,
             description=description,
             colour=PRIMARY_COLOUR,
+            **kwargs,
+        )
+
+
+class ErrorEmbed(Embed):
+    @overload
+    def __init__(self, description: str, **kwargs: Any) -> None: ...
+
+    @overload
+    def __init__(
+        self, title: str, description: str, **kwargs: Any
+    ) -> None: ...
+
+    def __init__(  # pyright:ignore[reportInconsistentOverload]
+        self, title: str, description: str | None = None, /, **kwargs: Any
+    ) -> None:
+        if description is None:
+            description = title
+            title = None
+
+        super().__init__(
+            title=title,
+            description=description,
+            colour=ERROR_COLOUR,
             **kwargs,
         )
