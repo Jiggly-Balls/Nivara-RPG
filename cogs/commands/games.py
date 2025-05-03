4a2fb6f4684e8f197dc70cf8c0f0c0cf1cecfc77
Jiggly Balls <jigglyballs9000@gmail.com>
2025-05-04 01:32:56 +0530

    Added roulette commands

--- cogs/commands/games.py
+++ cogs/commands/games.py
@@ -1,26 +1,33 @@
 import logging
-from pprint import pprint
 
 import discord
 from discord import ApplicationContext
-from discord.commands import option
 
-from backend.db_users import UserAspect, UserDB
+from backend.cache import Cache
 from core import BaseCog, Bot
-from core.views.games_view import MineGameView
+from core.utils import MainEmbed
+from core.views.games_view import (
+    MineGameView,
+    RouletteInitView,
+    roulette_embedder,
+)
 from data.games.mine import MineEngine
 
 logger = logging.getLogger(__name__)
 
 
 class Games(BaseCog):
-    def __init__(self, bot: Bot) -> None:
-        super().__init__(logger=logger)
-        self.bot = bot
-
     games_group = discord.SlashCommandGroup(
         "games", "Fun games to play with your friends!"
     )
+
+    roulette_group = games_group.create_subgroup(
+        "twisted-roulette", "Play the twisted roulette with your friends!"
+    )
+
+    def __init__(self, bot: Bot) -> None:
+        super().__init__(logger=logger)
+        self.bot = bot
 
     @games_group.command()
     async def mine(self, ctx: ApplicationContext) -> None:
@@ -30,33 +37,99 @@ class Games(BaseCog):
         image = miner.create_image()
         await ctx.respond(image, view=MineGameView(ctx.author.id, miner))
 
-    @games_group.command()
-    @option("id", required=False)
-    async def test2(self, ctx: ApplicationContext, id: str) -> None:
+    @roulette_group.command()
+    async def start(self, ctx: ApplicationContext) -> None:
         await ctx.defer()
 
-        id: int = int(id) if id else ctx.author.id
-        player = UserDB(id)
-        player_data = await player.get_account()
+        if ctx.guild.id in Cache.roulette_active:
+            return await ctx.respond(
+                embed=MainEmbed(
+                    "There is already an on going game in this server. Wait till it gets over to start a new one."
+                )
+            )
 
-        print(type(player_data))
-        print()
-        print()
-        pprint(dir(player_data))
-        print()
-        print()
-        pprint(player_data)
+        await ctx.respond(
+            embed=MainEmbed("Welcome to Twisted Russian Roulette!"),
+            view=RouletteInitView(ctx.author.id),
+        )
 
-        await ctx.followup.send(content="DONE")
-
-    @games_group.command()
-    async def test3(self, ctx: ApplicationContext, value: int) -> None:
+    @roulette_group.command()
+    async def join(self, ctx: ApplicationContext) -> None:
         await ctx.defer()
 
-        player = UserDB(ctx.author.id)
-        await player.update_aspect(UserAspect.exp, value=value)
+        if ctx.guild.id not in Cache.roulette_active:
+            return await ctx.followup.send(
+                embed=MainEmbed(
+                    "There is no active game in the current server."
+                ),
+                ephemeral=True,
+            )
 
-        await ctx.followup.send(content=f"Updated EXP to: {value}")
+        elif (
+            ctx.author
+            in Cache.roulette_active[ctx.guild.id]["players"].player_list
+        ):
+            return await ctx.followup.send(
+                embed=MainEmbed("You're already a part of the game."),
+                ephemeral=True,
+            )
+
+        elif Cache.roulette_active[ctx.guild.id]["start"]:
+            return await ctx.followup.send(
+                embed=MainEmbed(
+                    "You cannot join an on-going game. Wait till it gets over."
+                ),
+                ephemeral=True,
+            )
+
+        Cache.roulette_active[ctx.guild.id]["players"].player_list.append(
+            ctx.author
+        )
+        await ctx.followup.send(
+            embed=MainEmbed("You have successfully joined!"), ephemeral=True
+        )
+
+        await Cache.roulette_active[ctx.guild.id]["message"].edit(
+            embed=roulette_embedder(ctx.guild.id, ctx.author)
+        )
+
+    @roulette_group.command()
+    async def leave(self, ctx: ApplicationContext) -> None:
+        await ctx.defer()
+
+        if ctx.guild.id not in Cache.roulette_active:
+            return await ctx.send_followup(
+                embed=MainEmbed(
+                    "There is no active game in the current server."
+                ),
+                ephemeral=True,
+            )
+
+        elif (
+            ctx.author
+            not in Cache.roulette_active[ctx.guild.id]["players"].player_list
+        ):
+            return await ctx.send_followup(
+                embed=MainEmbed("You're not a part of the game yet."),
+                ephemeral=True,
+            )
+
+        elif Cache.roulette_active[ctx.guild.id]["start"]:
+            return await ctx.send_followup(
+                embed=MainEmbed("You cannot leave an on-going game."),
+                ephemeral=True,
+            )
+
+        Cache.roulette_active[ctx.guild.id]["players"].player_list.remove(
+            ctx.author
+        )
+        await ctx.send_followup(
+            embed=MainEmbed("You have left the game."), ephemeral=True
+        )
+
+        await Cache.roulette_active[ctx.guild.id]["message"].edit(
+            embed=roulette_embedder(ctx.guild.id, ctx.author)
+        )
 
 
 def setup(bot: Bot) -> None:
