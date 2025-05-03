d24646a2ff0de515fe1b99ed6e97db54378439b3
Jiggly Balls <jigglyballs9000@gmail.com>
2025-05-04 01:30:10 +0530

    Added roulette cache

--- backend/cache.py
+++ backend/cache.py
@@ -1,4 +1,13 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from discord import MISSING
+
+if TYPE_CHECKING:
+    from discord import Message
+
+    from data.games.roulette import ChamberIterator, PlayerIterator
 
 
 class Cache:
@@ -6,3 +15,7 @@ class Cache:
     last_reconnect: str = MISSING
     users: int = MISSING
     guilds: int = MISSING
+
+    roulette_active: dict[
+        int, dict[str, PlayerIterator | ChamberIterator | str | bool | Message]
+    ] = {}
