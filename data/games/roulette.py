5f4bead041570a28427f23083f2b4881b88e9b6b
Jiggly Balls <jigglyballs9000@gmail.com>
2025-05-04 11:01:43 +0530

    Added a dataclass: RouletteData instead of dict

--- data/games/roulette.py
+++ data/games/roulette.py
@@ -1,18 +1,28 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from typing import Self
 
-    from discord import Member
+    from discord import Member, Message, User
+
+
+@dataclass
+class RouletteData:
+    players: PlayerIterator
+    chamber: ChamberIterator
+    countdown: str
+    start: bool
+    message: Message
 
 
 class PlayerIterator:
-    def __init__(self, player_list: list[Member]) -> None:
+    def __init__(self, player_list: list[Member | User]) -> None:
         self.player_list = player_list
         self.current_player: Member = self.player_list[0]
-        self.dead_players: list[Member] = []
+        self.dead_players: list[Member | User] = []
         self.current_player_index: int = 0
 
     def __iter__(self) -> Self:
@@ -27,7 +37,7 @@ class PlayerIterator:
         if self.current_player_index > len(self.player_list) - 1:
             self.current_player_index = 0
 
-    def kill(self, player: Member) -> None:
+    def kill(self, player: Member | User) -> None:
         self.player_list.remove(player)
         self.dead_players.append(player)
         self._check_player()
